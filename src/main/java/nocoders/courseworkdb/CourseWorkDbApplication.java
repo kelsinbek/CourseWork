32825d9d54f155850a5f9cc02159e987c365fbf7
kelsinbek <janaevkelsinbek@gmail.com>
2023-03-26 04:59:24 +0600

    first commit

---
+++ src/main/java/nocoders/courseworkdb/CourseWorkDbApplication.java
@@ -0,0 +1,13 @@
+package nocoders.courseworkdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nocoders/courseworkdb/CourseWorkDbApplicationTests.java
@@ -0,0 +1,13 @@
+package nocoders.courseworkdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
