9c1b4620d09df9e142642ef29985b85ea7e2984c
kelsinbek <janaevkelsinbek@gmail.com>
2023-04-14 00:40:33 +0600

    second day

--- src/main/java/nocoders/courseworkdb/controllers/MainController.java
+++ src/main/java/nocoders/courseworkdb/controllers/MainController.java
@@ -1,17 +1,25 @@
-package nocoders.courseworkdb.controllers;
+  package nocoders.courseworkdb.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+  import lombok.RequiredArgsConstructor;
+  import org.springframework.stereotype.Controller;
+  import org.springframework.ui.Model;
+  import org.springframework.web.bind.annotation.GetMapping;
+  import org.springframework.web.bind.annotation.ModelAttribute;
+  import org.springframework.web.bind.annotation.PostMapping;
+  import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class MainController {
+  import java.io.IOException;
 
+  @Controller
+  @RequiredArgsConstructor
+  public class MainController {
     @GetMapping("/")
     public String login(Model model) {
-        model.addAttribute("title", "Главная страница");
-        return "register";
+      model.addAttribute("title", "Главная страница");
+      return "register";
+
     }
-}
+
+  }
 
 
--- src/main/java/nocoders/courseworkdb/enums/AccountType.java
+++ src/main/java/nocoders/courseworkdb/enums/AccountType.java
@@ -1,4 +1,18 @@
 package nocoders.courseworkdb.enums;
 
+
 public enum AccountType {
+    ADMIN("admin"),
+    STUDENT("student");
+
+    private final String accountType;
+
+    private AccountType(String accountType){
+        this.accountType=accountType;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
 }
+
