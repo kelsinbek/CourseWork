f153b0870824736f373a52bbbca7b7dbd52dbb6a
Mirlanbek <117597589+Mikilango@users.noreply.github.com>
2023-04-18 11:30:18 +0300

    commit

--- src/main/java/nocoders/courseworkdb/controllers/MainController.java
+++ src/main/java/nocoders/courseworkdb/controllers/MainController.java
@@ -1,31 +1,29 @@
-  package nocoders.courseworkdb.controllers;
+package nocoders.courseworkdb.controllers;
 
-  import lombok.RequiredArgsConstructor;
-  import org.springframework.stereotype.Controller;
-  import org.springframework.ui.Model;
-  import org.springframework.web.bind.annotation.GetMapping;
-  import org.springframework.web.bind.annotation.ModelAttribute;
-  import org.springframework.web.bind.annotation.PostMapping;
-  import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-  import java.io.IOException;
+@Controller
+public class MainController {
 
-  @Controller
-  @RequiredArgsConstructor
-  public class MainController {
+    @GetMapping("/")
+    public String profile(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "profile";
+    }
+
     @GetMapping("/register")
     public String register(Model model) {
-      model.addAttribute("title", "Главная страница");
-      return "register";
-
+        model.addAttribute("title", "Страница регистрации");
+        return "register";
     }
+
     @GetMapping("/login")
     public String login(Model model) {
-      model.addAttribute("title", "Главная страница");
-      return "login";
-
+        model.addAttribute("title", "Страница входа");
+        return "login";
     }
-
-  }
+}
 
 
