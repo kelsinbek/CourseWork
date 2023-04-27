a8c01c129997111d6f3ad2226cf6b0ddd5508c86
kelsinbek <janaevkelsinbek@gmail.com>
2023-04-27 23:01:23 +0600

    ...

--- src/main/java/nocoders/courseworkdb/configurations/WebConfig.java
+++ src/main/java/nocoders/courseworkdb/configurations/WebConfig.java
@@ -1,4 +1,21 @@
 package nocoders.courseworkdb.configurations;
 
-public class WebConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
 }
+
