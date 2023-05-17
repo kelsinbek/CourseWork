adf4130afa179b2a122544592eed94d2f3ec8a1d
kelsinbek <janaevkelsinbek@gmail.com>
2023-05-18 02:20:16 +0600

    Con

--- src/main/java/nocoders/courseworkdb/service/QuestionService.java
+++ src/main/java/nocoders/courseworkdb/service/QuestionService.java
@@ -4,6 +4,7 @@ import nocoders.courseworkdb.model.Question;
 import nocoders.courseworkdb.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -14,8 +15,18 @@ public class QuestionService {
         this.questionRepository = questionRepository;
     }
 
-    public List<Question> getQuestionsByQuizId(Long quizId) {
-        return questionRepository.findByQuizId(quizId);
+//    public List<Question> getQuestionsByQuizId(Long quizId) {
+//        return questionRepository.findByQuizId(quizId);
+//    }
+public List<Question> getQuestionsByQuizId(Long quizId) {
+    List<Question> questions = questionRepository.findByQuizId(quizId);
+    Collections.shuffle(questions); // Перемешиваем вопросы
+
+    if (questions.size() > 15) {
+        questions = questions.subList(0, 15); // Берем только первые 15 вопросов
     }
+
+    return questions;
+}
 }
 
